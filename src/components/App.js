ff9ce89ba00dc441f90530bb7a13c217970d3393
Youssef Benh <youssef.benhessou@gmail.com>
2020-04-04 23:32:45 -0400

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Input } from 'semantic-ui-react'
+
+function App() {
+  return (    
+    <div>
+      <Input size='massive' icon='search' placeholder='Search...' />
+    </div>
+  );
+}
+
+export default App;
